fc70a254ced4a09b455c7af1699397bdc9a2ef89
Hiroshi Nakahara <hiro116s@gmail.com>
2023-04-10 00:15:04 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
